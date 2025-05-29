de0e281416564e1099e3ed0c3471cb560456b44d
Thelma101 <akpata.thelma@gmail.com>
2025-05-29 20:47:03 +0100

    vendor category configuration

--- src/components/WeddingWebsite.tsx
+++ src/components/WeddingWebsite.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 const WeddingWebsite: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center ">
+    <div className="flex flex-col items-center justify-center mt-10 py-20 mx-auto px-2 md:px-24 ">
+
       <div className="flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold text-center text-bold">
-          Our Wedding Website
+        <h1 className="font-open-sans font-bold text-4xl text-center leading-normal text-gray-900 md:text-5xl lg:text-6xl xl:text-7xl ">
+          Create a free Website for your Wedding
         </h1>
-        <p className="text-xl text-center">
-          We have created a special website for our wedding, where you can find
-          all the details about the event, including the date, time, location,
-          and more. You can also RSVP to the wedding and share your thoughts and
-          photos with us.
-        </p>
+                            <p className="font-semibold text-7xl sm:text-lg md:text-2xl lg:text-3xl leading-tight">
+                        Chat with a Vendor
+                    </p>
         <a href="https://www.weddingwire.com/wedding-website/alex-and-ashley-wedding-website" target="_blank" rel="noopener noreferrer" className="mt-4 text-xl font-bold text-center  underline"></a>
       </div>
     </div>
