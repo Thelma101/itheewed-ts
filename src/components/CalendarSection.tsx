6a077e7be00774da20174a894120b0dda6da7428
Thelma101 <akpata.thelma@gmail.com>
2025-06-05 11:56:43 +0100

    calendar implementation

--- src/components/CalendarSection.tsx
+++ src/components/CalendarSection.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
+
+    interface Timeslot {
+        id: string;
+        time: string;
+        event: string;
+    }
 const CalendarSection = () => {
-    return (
+    const defaultTimeSlot: Timeslot[] = [
+        {
+            id: '1',
+            time: '10:00 AM',
+            event: 'Ceremony'
+        },
+        {
+            id: '2',
+            time: '11:00 AM',
+            event: 'Sermon'
+        },
+        {
+            id: '3',
+            time: '12:00 PM',
+            event: 'Blessing of Marriage'
+        }
+    ];
+
+    const [selectedDate, setSelectedDate] = useState<Date | null>()
+    const [timeSlots, setTimeSlots] = useState<Timeslot[]>()
+
+        return (
         <section className="flex bg-[#DBFCFF] text-primary-dark px-24">
             <div className="flex items-center justify-center py-20 gap-9">
                 <div className="flex flex-col items-start justify-center px-9 gap-5">
@@ -26,6 +53,9 @@ const CalendarSection = () => {
                     <div className="flex flex-col items-center justify-center bg-white rounded-xl border-[0.5px] border-primary-gray p-14 gap-11">
                         <div className="flex flex-col items-center justify-center">
                             <p className="text-2xl font-semibold leading-loose">Set your editable dates</p>
+                        </div>
+                        <div className="">
+
                         </div>
                     </div>
                 </div>
